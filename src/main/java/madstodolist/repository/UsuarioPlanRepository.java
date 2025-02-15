75cc8651594a97acfbdea4f4cc1a1790ba33384b
Jorgedehaz <jorgedehaz147@gmail.com>
2025-02-15 11:42:24 +0100

    feature(): Query UsuarioPlanRepository, template listaUsuariosPlanes.html, añadir redirección y autowire de repository en AdminController

--- src/main/java/madstodolist/repository/UsuarioPlanRepository.java
+++ src/main/java/madstodolist/repository/UsuarioPlanRepository.java
@@ -3,6 +3,7 @@ package madstodolist.repository;
 import madstodolist.model.UsuarioPlan;
 import madstodolist.model.UsuarioPlanId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public interface UsuarioPlanRepository extends JpaRepository<UsuarioPlan, UsuarioPlanId> {
     List<UsuarioPlan> findByUsuarioId(Long usuarioId);
+
+    @Query("SELECT up FROM UsuarioPlan up JOIN FETCH up.usuario u JOIN FETCH up.plan p")
+    List<UsuarioPlan> findAllUsuarioPlanes();
 }
 
 
