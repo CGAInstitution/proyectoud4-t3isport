7dec58442870a5a2461b63297657fab968387eb3
a23migueldss <miguids00@gmail.com>
2025-02-18 14:15:38 +0100

    feat(userHub.html): add content to premium methods

--- src/main/java/madstodolist/controller/UserHubController.java
+++ src/main/java/madstodolist/controller/UserHubController.java
@@ -1,7 +1,9 @@
 package madstodolist.controller;
 
 import madstodolist.dto.UsuarioData;
+import madstodolist.model.PlanesEntrenamiento;
 import madstodolist.model.UsuarioPlan;
+import madstodolist.service.PlanService;
 import madstodolist.service.UsuarioPlanService;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,6 +24,9 @@ public class UserHubController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private PlanService planService;
 
     @GetMapping("/usuarios/{id}/userhub")
     public String userHub(@PathVariable Long id, Model model, HttpSession session) {
@@ -35,7 +41,23 @@ public class UserHubController {
         if (usuario == null) {
             return "redirect:/login"; // Redirige si el usuario no existe
         }
+        List<PlanesEntrenamiento> planesPremium = new ArrayList<>();
+        PlanesEntrenamiento plan1= planService.findById(29L);
+        planesPremium.add(plan1);
+        PlanesEntrenamiento plan2= planService.findById(29L);
+        planesPremium.add(plan2);
+        PlanesEntrenamiento plan3= planService.findById(29L);
+        planesPremium.add(plan3);
 
+        List<PlanesEntrenamiento> planesGold = new ArrayList<>();
+        PlanesEntrenamiento plan4= planService.findById(29L);
+        planesGold.add(plan4);
+        PlanesEntrenamiento plan5= planService.findById(29L);
+        planesGold.add(plan5);
+        PlanesEntrenamiento plan6= planService.findById(29L);
+        planesGold.add(plan6);
+        model.addAttribute("planesPremium", planesPremium);
+        model.addAttribute("planesGold", planesGold);
         model.addAttribute("userId", sessionUserId);
         model.addAttribute("usuario", usuario); // Ahora Thymeleaf recibe directamente el objeto
 
